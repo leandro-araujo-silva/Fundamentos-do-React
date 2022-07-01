ce13c9449766c238cc6f9aa92af49e58e9d175b5
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2022-07-01 16:08:50 -0300

    Adicionando

---
+++ reactapp-ts/src/pages/Home/index.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+
+import { Card, CardProps } from '../../components/Card';
+
+export function Home() {
+
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState<CardProps[]>([])
+  const [user, setUser] = useState({name: '', avatar:''})
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+
+    setStudents(prevState => [...prevState, newStudent]);    // Pega o valor anterior e novo valor adicionado.
+  }
+
+  useEffect(() => {
+    // Corpo do useEffect
+    fetch('https://api.github.com/users/leandro-araujo-silva')
+    .then(response => response.json())
+    .then(data => {
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url
+      })
+    })
+  }, [])
+
+  return (
+    <div className='container'>
+      <header>
+        <h1>Lista de Presença</h1>
+
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil"/>
+        </div>
+      </header>
+      
+      <input 
+        type="text"
+        placeholder="Digite o seu nome..."
+        onChange={e => setStudentName(e.target.value)}
+      />
+
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {
+        students.map(student => ( 
+          <Card 
+            key={student.time}
+            name={student.name} 
+            time={student.time} 
+          />
+        ))
+      }
+      
+    </div>
+  )
+}
+
